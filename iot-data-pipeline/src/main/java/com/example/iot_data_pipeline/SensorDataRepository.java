7397e13323322e5d86723f805a0a040a942f4310
anusha-gangavarapu <ganusha999@gmail.com>
2025-05-02 20:31:10 +0530

    Delete iot-data-pipeline directory

--- iot-data-pipeline/src/main/java/com/example/iot_data_pipeline/SensorDataRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.iot_data_pipeline;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SensorDataRepository extends JpaRepository<SensorData, String> {
-}

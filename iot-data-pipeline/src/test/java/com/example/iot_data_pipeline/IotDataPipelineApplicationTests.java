7ce33bdcf25b2f0660ea6070a44f1560d353397f
anusha-gangavarapu <ganusha999@gmail.com>
2025-05-02 20:29:46 +0530

    Add files via upload
    Initial commit

---
+++ iot-data-pipeline/src/test/java/com/example/iot_data_pipeline/IotDataPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iot_data_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotDataPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
